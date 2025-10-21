11ff33278d27c4b5b848628f4b59a6d15519bb08
emanuel patriolino <adm.emanuel.santos@gmail.com>
2025-10-20 21:08:54 -0300

    recebendo os dados via insomnia, esperando o corpo todo do body antes de adicionar no db local, usando try catch e req.body, desestruturando o corpo, para então fazer o posh no array users

--- src/server.js
+++ src/server.js
@@ -1,27 +1,28 @@
 import http from "node:http"
 const users = []
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const {method, url} = req
+
+    const buffers = []
+
+    for await ( const chunk of req){
+        buffers.push(chunk)
+    }
+
+    try{
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
+
     if( method === "POST" && url === "/users"){
+        const {id, name, email } = req.body
         users.push(
             {
                 id:1,
-                nome:"emanuel",
-                email:"emanuel@@exemple.com",
-                idade:25
-            },
-            {
-                id:2,
-                nome:"vanessa",
-                email:"vanessa@@exemple.com",
-                idade:24
-            },
-            {
-                id:3,
-                nome:"arthur",
-                email:"arthur@@exemple.com",
-                idade:3
-            },
+                name,
+                email,
+            }
         )
         res.end()
         return;
